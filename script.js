4650d6ef20fd32402b799bfb4938c6601cf111ce
RichardyReis <richardyleonardoreis@gmail.com>
2023-02-02 18:01:54 -0300

    Primeiro projeto do curso de JavaScript do Curso em Vídeo

---
+++ script.js
@@ -0,0 +1,3 @@
+window.alert('Minha primeira mensagem');            //Gera uma mensagem na tela
+window.confirm('Está gostando de JavaScript?');     //Solicita uma confirmação
+window.prompt('Qual é o seu nome?');                //Gera um texto e uma caixa que possibilita o usuário de inserir um texto
